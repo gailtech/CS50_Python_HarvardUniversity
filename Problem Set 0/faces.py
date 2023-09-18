2e40693cc1c3c326f37dbda815a9489ed1d4a60e
gailtee <140375261+gailtech@users.noreply.github.com>
2023-09-18 19:23:28 +0800

    Create faces.py

---
+++ Problem Set 0/faces.py
@@ -0,0 +1,13 @@
+def main():
+    sentence = input("")
+    sentence = convert(sentence)
+    print(sentence)
+
+
+def convert(sentence):
+    sentence = sentence.replace(":)", "🙂")
+    sentence = sentence.replace(":(", "🙁")
+    return sentence
+
+
+main()
